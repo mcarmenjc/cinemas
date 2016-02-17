4dbae54614d0c965f21d24620a5d39a7c09c9db2
Maria Carmen Jimenez Campos <mcarmen.jimenez.campos@gmail.com>
2016-02-17 16:36:03 +0000

    beginning processing page, getting days, film names and times

--- scripts/controllers/odeon-schedule-controller.js
+++ scripts/controllers/odeon-schedule-controller.js
@@ -41,56 +41,9 @@ class OdeonScheduleController extends CinemaScheduleController {
 	getDate(dateText){
 		let regex = /[A-Za-z]+(\d+)\s(\w+)/gmi,
 			splitDate = regex.exec(dateText),
-			month = this.getMonthNumber(splitDate[2]),
+			month = super.getMonthNumber(splitDate[2]),
 			day = +splitDate[1];
 		return new Date(2016, month, day);
-	}
-
-
-	getMonthNumber(monthString){
-		let monthNumber;
-		switch(monthString.toUpperCase()){
-			case 'JAN':
-				monthNumber = 0;
-				break;
-			case 'FEB':
-				monthNumber = 1;
-				break;
-			case 'MAR':
-				monthNumber = 2;
-				break;
-			case 'APR':
-				monthNumber = 3;
-				break;
-			case 'MAY':
-				monthNumber = 4;
-				break;
-			case 'JUN':
-				monthNumber = 5;
-				break;
-			case 'JUL':
-				monthNumber = 6;
-				break;
-			case 'AUG':
-				monthNumber = 7;
-				break;
-			case 'SEP':
-				monthNumber = 8;
-				break;
-			case 'OCT':
-				monthNumber = 9;
-				break;
-			case 'NOV':
-				monthNumber = 10;
-				break;
-			case 'DEC':
-				monthNumber = 11;
-				break;
-			default:
-				monthNumber = -1;
-				break;
-		}
-		return monthNumber;
 	}
 }
 
--- scripts/controllers/playhouse-schedule-controller.js
+++ scripts/controllers/playhouse-schedule-controller.js
@@ -7,15 +7,47 @@ let Film = require('../models/film.js');
 
 class PlayhouseScheduleController extends CinemaScheduleController {
 	getSchedule(){
-		return requestp(this.cinema.url).then(body => {
+		return requestp(this.cinema.url + '/Whats_On').then(body => {
 			let $ = cheerio.load(body);
-			console.log(body);
 			let me = this;
-			$('#this-week').each(function(){
-				console.log(this.text());
+			let currentDay = undefined;
+			$('ul.whats-on').find('li').each(function(){
+				if ($(this).hasClass('heading dark')){
+					currentDay = me.getDate($(this).text());
+				}
+				else {
+					let name = this.getFilName(listElement, $),
+						times = me.getFilmTimes(listElement, $),
+						film;
+					if (times.length === 0){
+						//look for film in cinema film array
+						//if exists, append new day and times
+						//if not create a new film, add day and times and push it to the cinema films array
+					}
+				}
 			});
 			return this.cinema;
 		}).catch(error => {console.log(error)});
+	}
+
+	getDate(dateText){
+		let regex = /\w+\s+(\d+)\w+\s(\w+)/gmi,
+			splitDate = regex.exec(dateText),
+			month = super.getMonthNumber(splitDate[2]),
+			day = +splitDate[1];
+		return new Date(2016, month, day);
+	}
+
+	getFilmTimes(listElement, $){
+		let times=[];
+		$(listElement).find('dl.film-times').find('a.btn.btn-xs.btn-default.btn-special').each(function(){
+			times.push($(this).text());
+		});
+		return times;
+	}
+
+	getFilName(listElement, $){
+		return $(listElement).find('h2').text()	
 	}
 }
 
