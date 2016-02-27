1d5ab0b957fb69165b075c4662da2cf9753415b6
MCarmen Jimenez Campos <matrimc@gmail.com>
2016-02-27 15:53:09 +0000

    created google service class to get all cinemas by branch near London city cente

--- scripts/cinema-info.js
+++ scripts/cinema-info.js
@@ -6,5 +6,14 @@ let googleService = new GoogleService();
 googleService.getCinemasByBrand('odeon')
 .then(odeonCinemas => {
 	console.log(odeonCinemas.length);
-	console.log(odeonCinemas[0]);
+});
+
+googleService.getCinemasByBrand('picturehouse')
+.then(picturehouse => {
+	console.log(picturehouse.length);
+});
+
+googleService.getCinemasByBrand('cineworld')
+.then(cineworld => {
+	console.log(cineworld.length);
 });
--- scripts/services/google-service.js
+++ scripts/services/google-service.js
@@ -15,23 +15,62 @@ class GoogleService {
 
 	getCinemasByBrand(brand){
 		let url = this.getNearbyUrl(brand);
-		return request.get(url)
-		.then(resultJSON => {
-			let result = JSON.parse(resultJSON);
-			if (result.status === 'OK'){
-				return result.results;
+		let cinemaPlaces = [];
+		return this.nearBySearch(url)
+		.then(result => {
+			return this.processResult(result, cinemaPlaces, url);
+		})
+		.then(result => {
+			return this.processResult(result, cinemaPlaces, url);
+		})
+		.then(result => {
+			if (result !== undefined && result.status === 'OK'){
+				Array.prototype.push.apply(cinemaPlaces, result.results);
 			}
-			return [];
+			return cinemaPlaces;
 		})
 		.catch(error => {
 			console.error(error);
 		})
 	}
 
+	processResult(result, cinemas, url){
+		if (result !== undefined && result.status === 'OK'){
+			Array.prototype.push.apply(cinemas, result.results);
+			if (result.next_page_token !== undefined){
+				return this.nextNearBySearch(result.next_page_token, url);
+			}
+		}
+		return undefined;
+	}
+
+	nextNearBySearch(nextPageToken, url){
+		let nextPageUrl = url + '&pagetoken=' + encodeURI(nextPageToken),
+			timeoutPromise = new Promise(function(resolve, reject){
+				setTimeout(function(){
+					resolve();
+				}, 10000);
+			});
+		return timeoutPromise.then(() => {
+					return this.nearBySearch(nextPageUrl);	
+				});
+	}
+
+	nearBySearch(url){
+		return request.get(url)
+		.then(resultJSON => {
+			let result = JSON.parse(resultJSON);
+			return result;
+		})
+		.catch(error => {
+			console.error(error);
+		});
+	}
+
 	getNearbyUrl(name){
 		let url = this.baseUrl + 'nearbysearch/json?';
 		for(let key in this.parameters){
-			url = url + key + '=' + this.parameters[key] + '&';
+			url = url + key + '=' + encodeURI(this.parameters[key]) + '&';
 		}
 		url = url + 'name=' + name;
 		return url;
